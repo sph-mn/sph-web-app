b81669bed9b4c89cbd66947dd589f7a6b3080f02
sph-mn <sph@posteo.eu>
2017-07-15 16:06:27 +0000

    finish revise (sph web app client)

--- modules/sph/web/app/client.scm
+++ modules/sph/web/app/client.scm
@@ -28,7 +28,13 @@
 
   (define sph-web-app-client-description
     "client-code processing
-     transport protocol agnostic - just writes to port or returns the path of a prepared file")
+     transport protocol agnostic, it writes to a port or returns the path of a prepared file.
+     for the format for sources that can be passed see the documentation of (sph lang template)
+     (sph web app client) extends the template-source format to support non-list pairs as arguments:
+     example: (string:swa-path-suffix . relative-path)
+     the first element selects the swa-path to use based on its suffix.
+     source-path template: {swa-path}/client/{output-format}/{path-suffix}
+     destination-path template: {swa-root}/root/assets/{output-format}/_{sources-dependent-name}")
 
   ; -- path preparation
 
@@ -75,7 +81,8 @@
       (if path (string-append (first path) "/" (tail path)))))
 
   (define (path-pair->path-full swa-paths a format)
-    "(string:path-suffix . string:path) symbol -> false/string"
+    "(string:path-suffix . string:path) symbol -> false/string
+     suffix is of a swa-path"
     (let*
       ( (path-suffix (ensure-trailing-slash (first a)))
         (path-full
@@ -89,6 +96,7 @@
 
   (define* (prepare-sources swa-paths sources output-format #:optional (depth 0))
     "(string ...) list symbol [integer] -> any
+     normalise the (sph web app client) sources format to the (sph lang template) template-fold sources format.
      first level:
      * string: relative paths or full path
      * list: recurse once
@@ -108,16 +116,8 @@
 
   ;-- file processing
   ;
-  (define (has-suffix-proc suffix) (l (a) (if (string? a) (string-suffix? suffix a) #t)))
   (define default-env (apply environment (list-q (sph))))
   (define (search-env-path* a) (first-or-false (search-env-path (list a))))
-  (define path-uglifyjs (search-env-path* "uglifyjs"))
-
-  (define path-csstidy
-    ; there is also clean-css, but it does not format and cssbeautify-cli did not work at all
-    (search-env-path* "csstidy"))
-
-  (define path-html (search-env-path* "html"))
 
   (define (execute-files->port sources port executable . arguments)
     "(string ...) port string string ... ->
@@ -127,82 +127,112 @@
         (port-copy-all out port) (close-port out))
       executable arguments #t #t))
 
-  (define javascript-output-compress
-    (if path-uglifyjs
-      (l (sources port options) "hashtable (string:path ...) port:out -> boolean"
-        (execute-files->port sources port path-uglifyjs "--compress" "--mangle"))
-      #f))
-
-  (define javascript-output-format
-    (if path-uglifyjs
-      (l (sources port options) (execute-files->port sources port path-uglifyjs "--beautify")) #f))
-
-  (define css-output-compress
-    (if path-csstidy
-      (l (sources port options) (debug-log sources port options)
-        #;(execute-files->port sources port
-          path-csstidy "-" (cli-option "template" "highest") (cli-option "silent" "true")))
-      #f))
-
-  (define css-output-format
-    (if path-csstidy
-      (l (sources port options) (debug-log sources port options)
-        (execute-files->port sources port
-          path-csstidy "-" (cli-option "template" "default") (cli-option "silent" "true")))
-      #f))
-
-  (define html-output-format
-    (if path-html (l (sources port options) (execute-files->port sources port path-html)) #f))
-
-  (define (s-template-sxml->html sources port options)
+  (define (s-template-sxml->html source port options)
     "hashtable list port -> string
      compile sxml to xml from s-templates"
     (display "<!doctype html>" port)
     (template-fold (l (template-result . result) (sxml->xml template-result port) result)
       (and options (ht-ref options (q template-bindings)))
-      (or (and options (ht-ref options (q template-environment))) default-env) sources))
+      (or (and options (ht-ref options (q template-environment))) default-env) source))
 
   (define (s-template-sescript->javascript sources port options)
     "hashtable list port -> string
      compile sescript to javascript from s-templates"
     (let
       (sescript-load-paths
-        (or (and options (ht-ref options (q sescript-load-paths))) ses-default-load-paths))
+        (or (and options (ht-ref-q options sescript-load-paths)) ses-default-load-paths))
       (template-fold
         (l (template-result . result)
           (sescript->ecmascript template-result port sescript-load-paths) result)
-        (and options (ht-ref options (q template-bindings)))
-        (or (and options (ht-ref options (q template-environment))) default-env) sources)))
+        (and options (ht-ref-q options template-bindings))
+        (or (and options (ht-ref-q options template-environment)) default-env) sources)))
 
-  (define (s-template-plcss->css sources port options)
+  (define (s-template-plcss->css source port options)
     "hashtable list port -> string
      compile plcss to css from s-templates"
     (template-fold (l (template-result . result) (plcss->css template-result port) result)
-      (and options (ht-ref options (q template-bindings)))
-      (or (and options (ht-ref options (q template-environment))) default-env) sources))
+      (and options (ht-ref-q options template-bindings))
+      (or (and options (ht-ref-q options template-environment)) default-env) source))
 
   (define (string-and-suffix-proc suffix) (l (a) (and (string? a) (string-suffix? suffix a))))
 
   (define (string-and-suffix-or-true-proc suffix)
     (l (a) (if (string? a) (string-suffix? suffix a) #t)))
 
-  ; todo: either continue support of mode in asset-compiler, or create something here that selects it
-  (define (dispatch-output-processor mode)
-    (l () #t)
-    )
+  (define (development-mode? options) (eq? (q development) (ht-ref-q options mode)))
+
+  (define html-output-processor
+    (let (path-html (search-env-path* "html"))
+      (and path-html
+        (l (process-input out options)
+          (if (development-mode? options)
+            (execute-with-pipes
+              (l (child-in child-out) (begin-thread (process-input child-in) (close-port child-in))
+                (port-copy-all child-out out))
+              path-html (list) #t #t)
+            (process-input out))))))
+
+  (define css-output-processor
+    ; compress of format css depending on the option "mode"
+    (let
+      (path-csstidy
+        ; clean-css does not format and cssbeautify-cli did not work at all
+        (search-env-path* "csstidy"))
+      (and path-csstidy
+        (let
+          ( (format
+              (l (process-input out options)
+                (execute-with-pipes
+                  (l (child-in child-out)
+                    (begin-thread (process-input child-in) (close-port child-in))
+                    (port-copy-all child-out out))
+                  path-csstidy
+                  (list "-" (cli-option "template" "default") (cli-option "silent" "true")) #t #t)))
+            (compress
+              (l (process-input out options)
+                (execute-with-pipes
+                  (l (child-in child-out)
+                    (begin-thread (process-input child-in) (close-port child-in))
+                    (port-copy-all child-out out))
+                  path-csstidy
+                  (list "-" (cli-option "template" "highest") (cli-option "silent" "true")) #t #t))))
+          (l (process-input out-port options)
+            ((if (development-mode? options) format compress) process-input out-port options))))))
+
+  (define javascript-output-processor
+    ; compress of format javascript code depending on the option "mode"
+    (let (path-uglifyjs (search-env-path* "uglifyjs"))
+      (and path-uglifyjs
+        (let
+          ( (format
+              (l (process-input out options)
+                (execute-with-pipes
+                  (l (child-in child-out)
+                    (begin-thread (process-input child-in) (close-port child-in))
+                    (port-copy-all child-out out))
+                  path-uglifyjs (list "--beautify") #t #t)))
+            (compress
+              (l (process-input out options)
+                (execute-with-pipes
+                  (l (child-in child-out)
+                    (begin-thread (process-input child-in) (close-port child-in))
+                    (port-copy-all child-out out))
+                  path-uglifyjs (list "--compress" "--mangle") #t #t))))
+          (l (process-input out-port options)
+            ((if (development-mode? options) format compress) process-input out-port options))))))
+
+  (define (ac-input-copy source out options) (file->port source out))
 
   (define-as client-ac-config ht-create-symbol
     ; the main configuration for the asset pipeline
     html
-    (list (ht-create-symbol) (vector (q html) (string-and-suffix-proc ".html") ac-input-copy)
+    (list html-output-processor (vector (q html) (string-and-suffix-proc ".html") ac-input-copy)
       (vector (q sxml) (string-and-suffix-or-true-proc ".sxml") s-template-sxml->html))
     css
-    (list (ht-create-symbol production css-output-compress development css-output-format)
-      (vector (q css) (string-and-suffix-proc ".css") ac-input-copy)
+    (list css-output-processor (vector (q css) (string-and-suffix-proc ".css") ac-input-copy)
       (vector (q plcss) (string-and-suffix-or-true-proc ".plcss") s-template-plcss->css))
     javascript
-    (list
-      (ht-create-symbol production javascript-output-compress development javascript-output-format)
+    (list javascript-output-processor
       (vector (q javascript) (string-and-suffix-proc ".js") ac-input-copy)
       (vector (q sescript) (string-and-suffix-or-true-proc ".sjs") s-template-sescript->javascript)))
 
@@ -211,23 +241,26 @@
   (define (bindings-add-swa-env swa-env bindings)
     (pair (pair (q swa-env) swa-env) (or bindings (list))))
 
-  (define (client-port swa-env port-output output-format bindings sources)
+  (define (client-port output-format port-output swa-env bindings sources)
     "port symbol list:alist:template-variables list -> unspecified"
     (and-let* ((sources (prepare-sources (swa-env-paths swa-env) sources output-format)))
       (ac-compile client-ac-config output-format
-        (ht-ref (swa-env-config swa-env) (q mode) (q production)) sources
-        port-output (ht-create-symbol template-bindings (bindings-add-swa-env swa-env bindings)))))
+        sources port-output
+        (ht-create-symbol mode (ht-ref (swa-env-config swa-env) (q mode) (q production))
+          template-bindings (bindings-add-swa-env swa-env bindings)))))
 
   (define (client-file output-format swa-env bindings sources)
-    "symbol list:alist:template-variables list -> string:url-path"
+    "symbol list:alist:template-variables list -> string:url-path
+     sources are given in (sph lang template) template-fold source format"
     (and-let*
       ( (output-directory (client-output-directory (swa-env-root swa-env)))
-        (config (swa-env-config swa-env)) (mode (ht-ref-q config mode (q production)))
+        (config (swa-env-config swa-env))
+        (mode (ht-ref-q config mode (q production)))
         (when (ht-ref-q config client-file-when (if (eq? (q development) mode) (q always) (q new))))
         (sources (prepare-sources (swa-env-paths swa-env) sources output-format))
         (path
           (ac-compile->file client-ac-config output-format
-            mode sources
+             sources
             (string-append output-directory client-output-path) #:when
             when #:processor-options
             (ht-create-symbol template-bindings (bindings-add-swa-env swa-env bindings)))))
@@ -236,13 +269,13 @@
   (define (client-javascript swa-env port bindings . sources)
     "port list:alist:template-variables string ... -> unspecified
      like client-port with output format \"javascript\""
-    (client-port swa-env port (q javascript) bindings sources))
+    (client-port (q javascript) port swa-env bindings sources))
 
   (define (client-css swa-env port bindings . sources)
-    (client-port swa-env port (q css) bindings sources))
+    (client-port (q css) port swa-env bindings sources))
 
   (define (client-html swa-env port bindings . sources)
-    (client-port swa-env port (q html) bindings sources))
+    (client-port (q html) port swa-env bindings sources))
 
   (define (client-javascript-file swa-env bindings . sources)
     "list:alist:template-variables string ... -> url-path
--- modules/test/module/sph/web/app/client.scm
+++ modules/test/module/sph/web/app/client.scm
@@ -5,20 +5,29 @@
     (sph web app client)
     (sph web app start))
 
+  ; tests currently depend on csstidy and uglifyjs being available in PATH
+
   (define swa-env
-    (record swa-env-record "/tmp/" (list "/tmp/") (ht-create-symbol) (ht-create-symbol)))
+    (record swa-env-record "/tmp/"
+      (list "/tmp/") (ht-create-symbol mode (q production)) (ht-create-symbol)))
 
-  ;(client-html swa-env port bindings . sources)
-
- (define-test (client-html)
+  (define-test (client-html)
     (assert-equal "<!doctype html><div>test</div>"
-      (call-with-output-string
-        (l (out) (client-html swa-env out #f (list (list (q (div "test")))))))))
-
+      (call-with-output-string (l (out) (client-html swa-env out #f (list-q ((div "test"))))))))
 
   (define-test (client-css)
-    (assert-equal ""
+    (assert-equal "div{display:none;}\n"
       (call-with-output-string
-        (l (out) (client-css swa-env out #f (list (list (q (div "test")))))))))
+        (l (out) (client-css swa-env out #f (list-q ((("div" display none)))))))))
 
-  (test-execute-procedures-lambda client-html client-css))
+  (define-test (client-javascript)
+    (assert-equal "var a=3;\n"
+      (call-with-output-string
+        (l (out) (client-javascript swa-env out #f (list (list (list-q (define a 3)))))))))
+
+  (define-test (client-file)
+    (and-let* ((path (client-html-file swa-env #f (list-q ((div "test"))))))
+      (assert-equal "<!doctype html><div>test</div>"
+        (file->string (string-append (swa-env-root swa-env) "root" path)))))
+
+  (test-execute-procedures-lambda client-file client-javascript client-css client-html))
