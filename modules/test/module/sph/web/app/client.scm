d0fd2571b22126b7041e66d55564ffb3dbeebdc6
sph <sph@posteo.eu>
2017-09-29 09:26:28 +0000

    fix tests

--- modules/test/module/sph/web/app/client.scm
+++ modules/test/module/sph/web/app/client.scm
@@ -12,7 +12,7 @@
       (ht-create-symbol (q test) "/tmp/") (ht-create-symbol mode (q production)) (ht-create-symbol)))
 
   (define-test (client-html)
-    (assert-equal "<!doctype html><div>test</div>"
+    (assert-equal "<!doctype html><div>test</div>\n"
       (call-with-output-string
         (l (out) (client-html swa-env out #f (q test) (list-q ((div "test"))))))))
 
@@ -22,13 +22,13 @@
         (l (out) (client-css swa-env out #f (q test) (list-q ((("div" display none)))))))))
 
   (define-test (client-js)
-    (assert-equal "var a=3;\n"
+    (assert-equal "\"use strict\";var a=3;\n"
       (call-with-output-string
         (l (out) (client-js swa-env out #f (q test) (list (list (list-q (define a 3)))))))))
 
   (define-test (client-file)
     (and-let* ((path (client-file-html swa-env #f (q test) (list-q ((div "test"))))))
-      (assert-equal "<!doctype html><div>test</div>"
+      (assert-equal "<!doctype html><div>test</div>\n"
         (file->string (string-append (swa-env-root swa-env) "root" path)))))
 
   (test-execute-procedures-lambda client-file client-js client-css client-html))
